b8d758d91f16050e0b14fd0c9dfcac57b263cce3
pip-pipo <codemi692@gmail>
2021-04-13 10:51:25 -1100

    :bug: about readme added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 const ABOUT = styled.div`
   margin-top: 1rem;
@@ -11,8 +11,19 @@ const ABOUT = styled.div`
     align-items: center;
     background: #ffffff;
     color: #000000;
-    border-bottom:2px solid #ececee;
+    border-bottom: 2px solid #ececee;
   }
+
+
+.makehidden{
+  display:none;
+  /* visibility:hidden; */
+
+}
+
+
+
+
   .container {
     width: 70%;
     padding: 1rem;
@@ -21,36 +32,44 @@ const ABOUT = styled.div`
     line-height: 2rem;
     font-size: 1.5rem;
     color: #bbb3b3;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    flex-direction:column;
-    .ui{
-        i{
-            color:#242C37;
-              &:hover{
-                color:#0b0e11;
-            }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .ui {
+      i {
+        color: #242c37;
+        &:hover {
+          color: #0b0e11;
         }
+      }
     }
-    h5{
-         &:hover{
-          color:#706767;
+    h5 {
+      &:hover {
+        color: #706767;
       }
     }
   }
   #linkabout {
     color: #9e9393;
+    padding: 0.5rem 1rem;
+    text-decoration: underline;
+    margin: 0 1rem;
+    &:hover {
+      color: #066991;
+    }
   }
-  .gotoTOp{
-    position:fixed;
-    bottom:30px;
-    right:30px;
-    border-radius:2rem;
-      background: #3b3939;
-    padding:1.10rem;
-    &:hover{
-    background: #f7f7f7;
+
+  .gotoTOp {
+    border:none;
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    border-radius: 2rem;
+    background: #3b3939;
+    padding: 1.1rem;
+    &:hover {
+      background: #686b6d;
     }
   }
   @media screen and (max-width: 800px) {
@@ -60,7 +79,24 @@ const ABOUT = styled.div`
   }
 `;
 
+
+
 const About = () => {
+  const [readmorebutton,setreadmorebutton] = useState<null|boolean>(false)
+ 
+  const ReadMore = () => {
+    const addReadMore = document.getElementById("addReadMore");
+     addReadMore!.classList.toggle("makehidden")
+     if(addReadMore!.classList.contains("makehidden")){
+       setreadmorebutton(false);
+       console.log('hi')
+     }else{
+
+       setreadmorebutton(true);
+     }
+  };
+
+
   return (
     <ABOUT id="about">
       <div className="logo">
@@ -68,19 +104,38 @@ const About = () => {
       </div>
       <div className="container">
         <div className="ui">
-          <i className="fas fa-user-alt fa-7x" ></i>
+          <i className="fas fa-user-alt fa-7x"></i>
         </div>
         <h5>
-         Hi I am <strong>Morsalin..</strong> <br/>
-         <span>I am a Full Stack Web Developer & Programmer also A Freelancer .. specializiing in mostly JS, React, Mern-Stack, but also write Python, C++ and some other stuff.</span> <br/>
-          I build modern web applications professionally. I am a passionate full-stack software developer who  loves ❤  to learn and meet professionals from different areas. <br/> I will help you transform your personal or business concept into a successful based product.
-          <Link id="linkabout" to="/">
-            Read More
-          </Link>
+          Hi I am <strong>Morsalin..</strong> <br />
+          <span>
+            I am a Full Stack Web Developer & Programmer also A Freelancer ..
+            specializiing in mostly JS, React, Mern-Stack, but also write
+            Python, C++ and some other stuff.
+          </span>{" "}
+          <br />
+          I build modern web applications professionally. I am a passionate
+          full-stack software developer who loves ❤ to learn and meet
+          professionals from different areas. <br /> I will help you transform
+          your personal or business concept into a successful based product.
+          <div className="readmoreAdd" id="addReadMore">
+            <br/>
+            <br/>
+          Hi I am Morsalin..
+          I am a Full Stack Web Developer & Programmer also A Freelancer .. specializiing in mostly JS, React, Mern-Stack, but also write Python, C++ and some other stuff.
+          I build modern web applications professionally. I am a passionate full-stack software developer who loves ❤ to learn and meet professionals from different areas.
+          I will help you transform your personal or business concept into a successful based product.
+          hello
+      
+          </div>
+          <button onClick={ReadMore} id="linkabout" className="readmore">
+            {readmorebutton?"Expand Read More":"Read More"}
+          </button>
         </h5>
-
       </div>
-      <div className="gotoTOp"><i className="fas fa-angle-up fa-2x"></i></div>
+      <button className="gotoTOp">
+        <i className="fas fa-angle-up fa-2x"></i>
+      </button>
     </ABOUT>
   );
 };
